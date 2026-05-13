18c8ea6cf23212ec7fcd5fa30cae7da4a373fdd7
kauabarros-24 <martinsbarroskaua@gmail.com>
2026-05-13 19:00:37 -0300

    CHA

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-c5fd805d'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.hltr1e70ls"
+    "revision": "0.fbcmisj4go8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
