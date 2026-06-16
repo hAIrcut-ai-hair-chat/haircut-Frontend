3819820419554c3cac4298438260f9a05eb11164
kauabarros-24 <martinsbarroskaua@gmail.com>
2026-06-16 16:40:02 -0300

    CHORE: Reestablished system

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-c5fd805d'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.icmjopmu8g8"
+    "revision": "0.a6e7p5149i"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
